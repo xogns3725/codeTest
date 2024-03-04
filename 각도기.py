966f3ef69b12060c1020e45b777924c4e5a153de
xogns3725 <xogns3725@naver.com>
2024-03-04 12:50:46 +0900

    10문제 추가

---
+++ 각도기.py
@@ -0,0 +1,8 @@
+def solution(angle):
+    answer = (
+    1 if 0<angle<90 else
+    2 if angle == 90 else
+    3 if 90<angle<180 else
+    4 if angle == 180 else 0
+    )
+    return answer
---
+++ 나이 출력.py
@@ -0,0 +1,3 @@
+def solution(age):
+    # 태어났을 때 1살이므로 +1
+    return 2022-age+1
