775f8baccfe1490dc28a6ff245839ab3ac1fa6a8
xogns3725 <xogns3725@naver.com>
2024-03-05 10:07:55 +0900

    문자열안에 문자열.py 코드 간결하게 변경

--- 문자열안에 문자열.py
+++ 문자열안에 문자열.py
@@ -1,8 +1,6 @@
 def solution(str1, str2):
-    answer = 0
     # str1 안에 str2가 있는지 확인
     if str2 in str1:
-        answer = 1
-    else: answer = 2
-    return answer
+        return 1
+    else: return 2
 
